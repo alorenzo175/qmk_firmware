8cc280f96da59fbdbc12482b4eb92306239b7b65
alorenzo <alorenzo@isengard.lan>
2020-02-05 08:17:09 -0700

    keymap updates

--- keyboards/keebio/iris/keymaps/tony/keymap.c
+++ keyboards/keebio/iris/keymaps/tony/keymap.c
@@ -23,9 +23,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_ESC , KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, LALT_T(KC_LBRC),     KC_LGUI, KC_N,  KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC,
+     KC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, LALT_T(KC_LBRC),     KC_RGUI, KC_N,  KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                         LT(1,KC_BSPC), LCTL_T(KC_ENT), KC_ENT,                 KC_SPC, RALT_T(KC_SPC), LT(2,KC_BSPC)
+                                      MO(1), KC_LCTL, KC_LALT,                  KC_ENT,   KC_SPC, LT(2,KC_BSPC)
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
@@ -47,11 +47,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                              KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_PSLS, KC_PAST,   KC_P7,   KC_P8,   KC_P9,  KC_PDOT,                           KC_WH_U, KC_WH_L, KC_MS_U, KC_WH_R, KC_ACL2, KC_WBAK,
+     KC_SLSH,  KC_ASTR,  KC_7,   KC_8,    KC_9,    KC_DOT,                            KC_WH_U, KC_WH_L, KC_MS_U, KC_WH_R, KC_ACL2, KC_WBAK,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_PMNS, KC_PPLS,  KC_P4,   KC_P5,   KC_P6,  KC_PCMM,                            KC_WH_D, KC_MS_L, KC_MS_D, KC_MS_R, KC_ACL1,  KC_WFWD,
+     KC_MINS, KC_PLUS,  KC_4,    KC_5,    KC_6,    KC_EQL,                            KC_WH_D, KC_MS_L, KC_MS_D, KC_MS_R, KC_ACL1,  KC_WFWD,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     RESET,    KC_P0,   KC_P1,   KC_P2,   KC_P3,  KC_PEQL, KC_TRNS,          KC_TRNS, KC_STOP, KC_BTN1, KC_BTN2, KC_BTN3, KC_ACL0,  KC_TRNS,
+     RESET,    KC_0,    KC_1,    KC_2,    KC_3,   KC_COMM, KC_TRNS,          KC_TRNS, KC_STOP, KC_BTN1, KC_BTN2, KC_BTN3, KC_ACL0,  KC_TRNS,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                      TO(1),  KC_LGUI,  KC_TRNS,                  KC_TRNS, KC_TRNS,  TO(0)
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
